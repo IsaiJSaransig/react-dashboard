29b839604b76ee3246f181cd97b79e74f8977288
Isai Saransig <isaisaransig@gmail.com>
2024-05-01 23:59:13 -0500

    change logo

--- src/Components/OverviewCard.jsx
+++ src/Components/OverviewCard.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import upRow from '../assets/images/icon-up.svg'
 import downRow from '../assets/images/icon-down.svg'
 import facebookLogo from '../assets/images/icon-facebook.svg'
+import twitterLogo from '../assets/images/icon-twitter.svg'
+import instagramLogo from '../assets/images/icon-instagram.svg'
+import youtubeLogo from '../assets/images/icon-youtube.svg'
+
 const networkColor = {
   Facebook: 'bg-Facebook',
   Twitter: 'bg-Twitter',
   Instagram: 'bg-Instagram-gradient',
   YouTube: 'bg-YouTube',
+}
+
+const networkIcon = {
+  Facebook: facebookLogo,
+  Twitter: twitterLogo,
+  Instagram: instagramLogo,
+  YouTube: youtubeLogo,
 }
 
 function toThousand(number) {
@@ -21,7 +32,7 @@ export const OverviewCard = ({ network, username, audience, audienceType, today,
     <article className='bg-Light-Grayish-Blue w-full h-[216px] rounded-[10px] mb-4 mx-auto overflow-hidden text-center dark:bg-Dark-Desaturated-Blue hover:brightness-95 hover:cursor-pointer dark:hover:brightness-125'>
       <div className={`${networkColor[network]} rounded-t-[10px]  h-1.5 mb-8`}></div>
       <div className='flex items-center place-content-center mb-[28px] gap-2'>
-        <img src={facebookLogo} />
+        <img src={`${networkIcon[network]}`} />
         <p className=' text-x text-Dark-Grayish-Blue font-bold'>{username}</p>
       </div>
       <p className='text-5xl text-Dark-Blue font-bold mb-2 dark:text-white'>{toThousand(audience)}</p>
--- src/Components/OverviewTodayCard.jsx
+++ src/Components/OverviewTodayCard.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import upRow from '../assets/images/icon-up.svg'
 import downRow from '../assets/images/icon-down.svg'
+import facebookLogo from '../assets/images/icon-facebook.svg'
+import twitterLogo from '../assets/images/icon-twitter.svg'
+import instagramLogo from '../assets/images/icon-instagram.svg'
+import youtubeLogo from '../assets/images/icon-youtube.svg'
+const networkIcon = {
+    Facebook: facebookLogo,
+    Twitter: twitterLogo,
+    Instagram: instagramLogo,
+    YouTube: youtubeLogo,
+  }
+
 function toThousand(number) {
     if (number > 10000) {
         return `${(number / 1000)}K`
@@ -14,7 +25,7 @@ export const OverviewTodayCard = ({ network, statsType, stats, isUp, image, stat
         <article className='bg-Light-Grayish-Blue w-full h-[126px] rounded-[10px] mb-4 mx-auto dark:bg-Dark-Desaturated-Blue hover:brightness-95 hover:cursor-pointer dark:hover:brightness-125 py-5 px-7'>
             <div className='flex justify-between items-center'>
                 <p className='text-Dark-Grayish-Blue font-semibold'>{statsType}</p>
-                <img src={image} alt="" />
+                <img src={networkIcon[network]} alt="" />
             </div>
 
             <div className='flex items-center justify-between mt-4'>
